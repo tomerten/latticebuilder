5b5d028c66ada7862a948b0e6ddaa897af593554
Tom Mertens <tom.mertens@helmholtz-berlin.de>
2021-05-01 11:53:43 +0200

    started updating test

--- tests/test_latticebuilder.py
+++ tests/test_latticebuilder.py
@@ -1,5 +1,5 @@
-from latticebuilder import __version__
+from latticebuilder import LatticeBuilderLine, __version__
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
